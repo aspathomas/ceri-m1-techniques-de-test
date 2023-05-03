aacae938bc3feb3da2fa2b1df6c56cd62a424e48
thomas_aspa <aspa.thomas@gmail.com>
2023-05-03 16:24:58 +0200

    TP5 v2

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,10 +1,8 @@
 package fr.univavignon.pokedex.api;
 import org.junit.Test;
 import org.mockito.Mockito;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
 
 public class IPokedexFactoryTest {
 
@@ -12,11 +10,8 @@ public class IPokedexFactoryTest {
     public void testCreatePokedex() throws PokedexException {
         IPokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
         IPokemonFactory pokemonFactory = new PokemonFactory();
-
         IPokedexFactory pokedexFactory = new PokedexFactory();
-
         IPokedex pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
-
         assertNotNull(pokedex);
         assertEquals(Pokedex.class, pokedex.getClass());
     }
